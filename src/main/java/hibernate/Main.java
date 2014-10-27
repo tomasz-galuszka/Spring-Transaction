c8cc783a02885a106c7b2c0ab008b8c59ab844dd
tomasz <tomgal144@gmail.com>
2014-10-27 22:44:28 +0100

    hibernate

--- src/main/java/hibernate/Main.java
+++ src/main/java/hibernate/Main.java
@@ -1,10 +1,11 @@
-package jdbc;
+package hibernate;
 
+import hibernate.beans.Table1;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
@@ -15,18 +16,21 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"context.xml"});
+        HibernateTransactionManager tManager = (HibernateTransactionManager) ctx.getBean("txManager2");
 
-        DataSourceTransactionManager txManager = (DataSourceTransactionManager) ctx.getBean("txManager");
         DefaultTransactionDefinition tDef = new DefaultTransactionDefinition();
-        TransactionStatus tStatus = txManager.getTransaction(tDef);
+        TransactionStatus tStatus = tManager.getTransaction(tDef);
         try {
-            JdbcTemplate jdbcTemplate = new JdbcTemplate(txManager.getDataSource());
-            jdbcTemplate.execute("INSERT INTO table1(name) VALUES('Transaction completed !');");
 
-        } catch (Exception ex) {
-            txManager.rollback(tStatus);
+            HibernateTemplate hibernateTemplate = new HibernateTemplate(tManager.getSessionFactory());
+            Table1 test1 = new Table1();
+            test1.setName("Hibernate transaction completed !");
+            hibernateTemplate.save(test1);
+
+        } catch (Exception e) {
+            tManager.rollback(tStatus);
         }
-        txManager.commit(tStatus);
+        tManager.commit(tStatus);
 
         ((AbstractApplicationContext) ctx).registerShutdownHook();
     }
---
+++ src/main/java/hibernate/beans/Table1.java
@@ -0,0 +1,26 @@
+package hibernate.beans;
+
+/**
+ * Created by tomasz on 27.10.14.
+ */
+public class Table1 {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
